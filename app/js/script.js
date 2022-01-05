ae316d6bed4b96abfedb632041cf7622cb972512
Samuel Oyewole <findseunoyewole@gmail.com>
2022-01-04 23:05:22 -0800

    footer, reviews

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,32 @@
-console.log('world!!')
+const btnHambugger = document.querySelector('#btnHambugger')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+const fadeElems = document.querySelectorAll('.has-fade')
+const body = document.querySelector('body')
+
+
+
+btnHambugger.addEventListener(
+'click', () => {
+    if(header.classList.contains('open')){
+
+        body.classList.remove('noscroll');
+        header.classList.remove('open')
+        fadeElems.forEach(element => {
+            element.classList.remove('fade-in')
+            element.classList.add('fade-out')
+        })
+    }
+
+    else{
+        body.classList.add('noscroll');
+        header.classList.add('open')
+        fadeElems.forEach(element => {
+            element.classList.add('fade-in')
+            element.classList.remove('fade-out')
+        }  )
+
+    }
+}
+
+)
