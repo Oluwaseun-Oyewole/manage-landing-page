7ada955a15fcafe720da0fe220f3f9c6ee228a2f
Samuel Oyewole <findseunoyewole@gmail.com>
2022-01-16 16:27:15 -0800

    mobile menu modified

--- app/js/script.js
+++ app/js/script.js
@@ -1,32 +1,77 @@
-const btnHambugger = document.querySelector('#btnHambugger')
+// const btnHambugger = document.querySelector('#btnHambugger')
+// const header = document.querySelector('.header')
+// const overlay = document.querySelector('.overlay')
+// const fadeElems = document.querySelectorAll('.has-fade')
+// const body = document.querySelector('body')
+
+
+
+// btnHambugger.addEventListener(
+// 'click', () => {
+//     if(header.classList.contains('open')){
+
+//         body.classList.remove('noscroll');
+//         header.classList.remove('open')
+//         fadeElems.forEach(element => {
+//             element.classList.remove('fade-in')
+//             element.classList.add('fade-out')
+//         })
+//     }
+
+//     else{
+//         body.classList.add('noscroll');
+//         header.classList.add('open')
+//         fadeElems.forEach(element => {
+//             element.classList.add('fade-in')
+//             element.classList.remove('fade-out')
+//         }  )
+
+//     }
+// }
+
+// )
+
+
+
+const hambugger = document.querySelector('.hambugger')
 const header = document.querySelector('.header')
 const overlay = document.querySelector('.overlay')
-const fadeElems = document.querySelectorAll('.has-fade')
 const body = document.querySelector('body')
+const fadeElems = document.querySelectorAll('.has-fade')
+const mobilemenu = document.querySelector('.mobilemenu')
 
 
 
-btnHambugger.addEventListener(
-'click', () => {
+
+hambugger.addEventListener('click', () => {
+
     if(header.classList.contains('open')){
-
-        body.classList.remove('noscroll');
         header.classList.remove('open')
-        fadeElems.forEach(element => {
-            element.classList.remove('fade-in')
-            element.classList.add('fade-out')
-        })
-    }
+        body.classList.remove('noscroll')
+        overlay.classList.remove('fade-in')
+        overlay.classList.add('fade-out')
+        mobilemenu.classList.remove('mobile-fadeIn')
+        mobilemenu.classList.add('mobile-fadeOut')
 
+
+        // fadeElems.forEach(element => {
+        //     element.classList.remove('fade-in')
+        //     element.classList.add('fade-out')
+        // })
+
+    }
     else{
-        body.classList.add('noscroll');
-        header.classList.add('open')
-        fadeElems.forEach(element => {
-            element.classList.add('fade-in')
-            element.classList.remove('fade-out')
-        }  )
+        body.classList.add('noscroll')
+        header.classList.add('open')  
+        overlay.classList.remove('fade-out')
+        overlay.classList.add('fade-in')
+        mobilemenu.classList.add('mobile-fadeIn')
+        mobilemenu.classList.remove('mobile-fadeOut')
+        
 
+        // fadeElems.forEach(element => {
+        //     element.classList.add('fade-in')
+        //     element.classList.remove('fade-out') 
+        // })
     }
-}
-
-)
+})
